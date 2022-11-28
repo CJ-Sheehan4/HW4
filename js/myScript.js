d3c324ca951c9d2ab353756f6ff75ac08af86cd3
CJ Sheehan <SheehanCorn@gmail.com>
2022-11-28 13:13:37 -0500

    validation of floats

--- js/myScript.js
+++ js/myScript.js
@@ -13,24 +13,32 @@ sources: https://jqueryvalidation.org/validate/
             https://jqueryvalidation.org/jQuery.validator.addMethod/
             https://jqueryvalidation.org/category/methods/
 */
+//This function gets all input from the input boxes and returns 
+// an objects with key values pairs that are arrays
+// of the corresponding value and its actual element
 function getInputValues(){
   var minColElement = document.getElementById("minc");
-  var minColVal = parseInt(document.getElementById("minc").value);
+  var minColVal = document.getElementById("minc").value;
   var maxColElement = document.getElementById("maxc");
-  var maxColVal = parseInt(document.getElementById("maxc").value);
+  var maxColVal = document.getElementById("maxc").value;
   var minRowElement = document.getElementById("minr");
-  var minRowVal = parseInt(document.getElementById("minr").value);
+  var minRowVal = document.getElementById("minr").value;
   var maxRowElement = document.getElementById("maxr");
-  var maxRowVal = parseInt(document.getElementById("maxr").value);
+  var maxRowVal = document.getElementById("maxr").value;
   var input = {minCol: [minColVal, minColElement], 
               maxCol: [maxColVal, maxColElement],
               minRow: [minRowVal, minRowElement],
               maxRow: [maxRowVal, maxRowElement]};
   return input;
   }
+  //generates the table the same as in HW3
 function generateTable(input){
   var table = document.getElementById("table");
   table.remove();
+  input.minCol[0] = parseInt(input.minCol[0]);
+  input.maxCol[0] = parseInt(input.maxCol[0]);
+  input.minRow[0] = parseInt(input.minRow[0]);
+  input.maxRow[0] = parseInt(input.maxRow[0]);
   // creates new HTML table
   var tableParentDiv = document.getElementById("coltable");
   var newTable = document.createElement("table");
@@ -49,11 +57,8 @@ function generateTable(input){
   var tHeadtBodyPair = document.getElementById("table").children;
   var tHead = tHeadtBodyPair[0];
   var trCollection = tHead.children;
-  console.log(trCollection);
   var tableColHeaders = trCollection[0].children;
-  console.log(tableColHeaders);
   tableColHeaders[1].innerHTML = input.minCol[0];
-  console.log(tableColHeaders[1]);
   // populating the top row/column headers
   for(var i = input.minCol[0] + 1; i <= input.maxCol[0]; i++) {
     var newTh = document.createElement("th");
@@ -61,7 +66,6 @@ function generateTable(input){
     newTh.appendChild(textNode);
     trCollection[0].appendChild(newTh);
   }
-  console.log("input.maxCol[0]: " + input.maxCol[0] + ", " + typeof(input.maxCol[0]));
   //populating each row after the top row
   // this for loop creates a new row element
   // and then the inner for-loop populates the rest of that row 
@@ -82,58 +86,75 @@ function generateTable(input){
   }
 }
 /*main*/ 
-//$('#table').hide();
+$('#table').hide();
 $(document).ready(function(){
+  // validation for the form using jquery validation
   $("#form").validate({
+    // declaring the error class so elements appear red
     errorClass: "error",
-    //focusCleanup: true,
+    // set of rules for each of my four input elements
+    // All four look the same except for max column and max row have an added method
     rules: {
+      // minimum column 
       minc: {
-        required: true,        
+        // the field is required to have input
+        required: true,
+        // the entered text cannot have whitespace
         nowhitespace: true,
+        // the field must be a number, opposed to entering characters
         number: true,
+        // the range of the input entered must be between -50 and 50
         range: [-50,50],
-        //mincIsSmallest: true
+        onlyIntegers: true
       },
+      // maximum column 
       maxc: {
         required: true,        
         nowhitespace: true,
         number: true,
         range: [-50,50],
-        maxcIsLargest: true
+        // this is a defined method at the bottom of script to prevent the 
+        // max column being smaller than the min column
+        maxcIsLargest: true,
+        onlyIntegers: true
       },
       minr: {
         required: true,        
         nowhitespace: true,
         number: true,
-        range: [-50,50]
+        range: [-50,50],
+        onlyIntegers: true
       },
       maxr: {
         required: true,        
         nowhitespace: true,
         number: true,
         range: [-50,50],
-        maxrIsLargest: true
+        maxrIsLargest: true,
+        onlyIntegers: true
       }
     },
+    // the submitHander gets all the input from the input element and generates the tables with it
     submitHandler: function(){
       var input = getInputValues();
-      console.log(input);
       generateTable(input);
     }
   });
 });
+// defined method to to ensure that the max column is larger than the min column
+// the value passed is the value from the max column, which is then compared 
+// to the input that is in the min column. It returns false if max column is >= to min column
+// which will then display that error message string that is the third parameter of the addMethod function. 
 $.validator.addMethod("maxcIsLargest", function(value){
   var input = getInputValues();
-  return input.minCol[0] < value;
+  return parseInt(input.minCol[0]) <= parseInt(value);
 }, "minimum column must be less than maximum column");
+// defined method to to ensure that the max row is larger than the min row
 $.validator.addMethod("maxrIsLargest", function(value){
   var input = getInputValues();
-  return input.minRow[0] < value;
+  return parseInt(input.minRow[0]) <= parseInt(value);
 }, "minimum row must be less than maximum row");
-
-
-/*
-either reject or round for floats
-maybe take away focus
-*/
+// method to ensure that only integers are entered as a value
+$.validator.addMethod("onlyIntegers", function(value){
+  return !(value.includes('.'));
+}, "only integer values allowed");
